bf7b5b66ddbda98a6dd0ee099e4f6f076941ed56
Brent Aronsen <bkaronsen@gmail.com>
2021-12-24 17:48:37 -0600

    Finish cell selection and highlighting

--- src/components/Games/Crossword.js
+++ src/components/Games/Crossword.js
@@ -21,136 +21,152 @@ for (var i = 1; i < 26; i++) {
 const initialState = {
   cellData: DUMMYDATA,
   selectedCell: 0,
-  across: true
-}
+  across: true,
+};
+
+const updateHighlighting = (state, index) => {
+  const cellNum = index + 1;
+  if (state.across) {
+    //highlight letters in same word (across)
+    //first, check to the right
+    index++;
+    while (index % 5 > 0) {
+      if (state.cellData[index].disabled) {
+        break;
+      }
+      state.cellData[index].highlight = true;
+      index++;
+    }
+    //second, check to the left
+    index = cellNum - 1;
+    index--;
+    while (index % 5 !== 4 && index >= 0) {
+      if (state.cellData[index].disabled) {
+        break;
+      }
+      state.cellData[index].highlight = true;
+      index--;
+    }
+  } else {
+    //highlight letters in same word (down)
+    //first, check below
+    index += 5;
+    while (index < 25) {
+      if (state.cellData[index].disabled) {
+        break;
+      }
+      state.cellData[index].highlight = true;
+      index += 5;
+    }
+    //second, check above
+    index = cellNum - 1;
+    index -= 5;
+    while (index >= 0) {
+      if (state.cellData[index].disabled) {
+        break;
+      }
+      state.cellData[index].highlight = true;
+      index -= 5;
+    }
+  }
+  return state.cellData;
+};
+
+//reset highlight and focus for all cells
+const clearCellDisplay = (state) => {
+  return state.cellData.map((cell) => ({
+    ...cell,
+    highlight: false,
+    focus: false,
+  }));
+};
+
+const getNextCell = (state, index) => {
+  if (state.across) {
+    index++;
+    if (index === 25) {
+      index = 0;
+    }
+    while (index < 25) {
+      if (state.cellData[index].disabled === "") {
+        break;
+      }
+      index++;
+      if (index === 24) {
+        index = 0;
+      }
+    }
+  } else { //down
+    index += 5;
+    if (index === 24 + 5) {
+      index = 0;
+    } else if (index > 24) {
+      index -= 24;
+    } 
+    while (index < 25) {
+      if (state.cellData[index].disabled === "") {
+        break;
+      }
+      index += 5;
+      if (index > 24) {
+        index -= 24;
+      }
+    }
+
+
+  }
+  return index;
+};
 
 const reducer = (state, action) => {
   const cellNum = parseInt(action.event.target.dataset.cellnum);
   let index = cellNum - 1;
 
   switch (action.type) {
-    case 'keydown':
-  
+    case "keydown":
       //NEED TO ADD CHECK FOR LETTER!
       state.cellData[index].value = action.event.key;
-      const newCellData = state.cellData.map((cell) => ({
-        ...cell,
-        focus: false, //set all other cells' focus to false
-      }));
-      //NEED TO ADD DOWN!! AND REFACTOR THIS
-      //NEED TO FIX HIGHLIGHTING AFTER IT SELECTS NEW CELL!
-      //pull mousedown stuff into a function, yo.
-      if (state.across) {
-        index++;
-        if (index === 25) {
-          index = 0;
-        }
-        while (index < 25) {
-          if (newCellData[index].disabled === '') {
-            break;
-          }
-          index++;
-          if (index === 24) {
-            index = 0;
-          }
-        }
-        newCellData[index].focus = true;
-        return {
-          cellData: newCellData,
-          selectedCell: index + 1,
-          across: state.across
-        }
-      } else {
-        //DOWN!
-        return {
-          cellData: state.cellData,
-          selectedCell: state.selectedCell,
-          across: state.across
-        }
-      }
+      state.cellData = clearCellDisplay(state);
+      index = getNextCell(state, index);
+      state.cellData[index].focus = true;
+      state.cellData = updateHighlighting(state, index);
 
-    case 'mousedown':
-  
+      return {
+        cellData: state.cellData,
+        selectedCell: index + 1,
+        across: state.across,
+      };
+
+    case "mousedown":
       if (cellNum === state.selectedCell) {
         state.across = !state.across;
       }
-      // state.selectedCell = cellNum;
-  
-      //Highlight cells in same word (either across or down)
-      const updCellData = state.cellData.map((cell) => ({
-        ...cell,
-        highlight: false, //set all other cells' highlight to false
-        focus: false
-      }));
-      updCellData[index].focus = true;
 
-      if (state.across) {
-        //highlight letters in same word (across)
-        //first, check to the right
-        index++;
-        while (index % 5 > 0) {
-          if (updCellData[index].disabled) {
-            break;
-          }
-          updCellData[index].highlight = true;
-          index++;
-        }
-        //second, check to the left
-        index = cellNum - 1;
-        index--;
-        while (index % 5 !== 4 && index >= 0) {
-          if (updCellData[index].disabled) {
-            break;
-          }
-          updCellData[index].highlight = true;
-          index--;
-        }
-      } else {
-        //first, check below
-        index += 5;
-        while (index < 25) {
-          if (updCellData[index].disabled) {
-            break;
-          }
-          updCellData[index].highlight = true;
-          index += 5;
-        }
-        //second, check above
-        index = cellNum - 1;
-        index -= 5;
-        while (index >= 0) {
-          if (updCellData[index].disabled) {
-            break;
-          }
-          updCellData[index].highlight = true;
-          index -= 5;
-        }
-      }
-      
-      //FIX THIS!
-      // updCellData[index].focus = true;
+      state.cellData = clearCellDisplay(state);
+      state.cellData[index].focus = true;
+      state.cellData = updateHighlighting(state, index);
+
       return {
-        cellData: updCellData,
+        cellData: state.cellData,
         selectedCell: cellNum,
-        across: state.across
-      }
+        across: state.across,
+      };
     default:
       throw new Error();
   }
-}
+};
 
 const Crossword = (props) => {
-
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  console.log('rendered Crossword');
+  console.log("rendered Crossword");
 
   const onKeyDownHandler = (event) => {
-    dispatch({type: 'keydown', event})
-  }
+    dispatch({ type: "keydown", event });
+  };
 
   const onMouseDownHandler = (event) => {
-    dispatch({type: 'mousedown', event})
+    dispatch({ type: "mousedown", event });
   };
 
   return (
