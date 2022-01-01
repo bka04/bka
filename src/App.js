e3724c9b8a1e71d15dad113def199532504a2471
Brent Aronsen <bkaronsen@gmail.com>
2021-12-31 18:46:35 -0600

    Refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
-import { Route, Redirect, Switch } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 
 import Header from "./components/layout/Header";
 import Books from "./pages/Books";
 import Games from "./pages/Games";
 
+const Acrobatics = () => <p>Acrobatics stuff coming soon. Possibly slideshow with pause/forward/back buttons.</p>;
+const Dev = () => <p>Development stuff coming soon</p>;
+
+const routes = [
+  {path: '/'},
+  {path: '/books', Component: Books},
+  {path: '/games', Component: Games},
+  {path: '/acrobatics', Component: Acrobatics},
+  {path: '/development', Component: Dev}
+]
+
 function App() {
   return (
     <div>
       <Header />
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/books" />
+
+      {routes.map(({path, Component}) => (
+        <Route key={path} path={path} exact>
+          {path === '/' ? <Redirect to='/books' /> : <Component />}
         </Route>
-        <Route path="/books">
-          <Books />
-        </Route>
-        <Route path="/games">
-          <Games />
-        </Route>
-        <Route path="/acrobatics">
-          <p>Acrobatics stuff coming soon</p>
-        </Route>
-        <Route path="/development">
-          <p>Development stuff coming soon</p>
-        </Route>
-        <Route path='*'>
-          <p>Path not found</p>
-        </Route>
-      </Switch>
+      ))}
     </div>
   );
 }
