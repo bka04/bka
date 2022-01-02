f306a1fc0b6d7d56ff4ecd1cc459e9e222650e1e
Brent Aronsen <bkaronsen@gmail.com>
2022-01-02 11:40:11 -0600

    Add menu button to header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,43 @@
 import { NavLink } from "react-router-dom";
+import { useState, useEffect, useCallback } from "react";
 
 import classes from "./Header.module.css";
 
 const Header = (props) => {
+  const [showNavLinks, setShowNavLinks] = useState(true);
+
+  const onClickHandler = () => {
+    setShowNavLinks((prevState) => {
+      return !prevState;
+    })
+  };
+
+  const showHideNavLinks = useCallback(() => {
+    if (window.innerWidth <= 500) {
+      // setShowNavLinks((prevState) => {
+      //   return !prevState;
+      // });
+    } else {
+      setShowNavLinks(true);
+    } 
+  }, []);
+
+  useEffect(() => {
+    showHideNavLinks();
+  }, [showHideNavLinks])
+
+
+  const ulClass = showNavLinks ? classes.showNavLinks : classes.hideNavLinks;
+
+  window.addEventListener('resize', showHideNavLinks);
+
   return (
     <header>
       <nav className={classes.nav}>
-        <ul>
+        <div>
+          <span className={`material-icons ${classes.menuIcon}`} onClick={onClickHandler}>menu</span>
+        </div>
+        <ul className={ulClass}>
           <li>
             <NavLink to="/books" activeClassName={classes.active}>
               Books
