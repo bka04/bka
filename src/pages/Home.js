ff9a1e6256460d957da9895160336c82a1afdc68
Brent Aronsen <bkaronsen@gmail.com>
2022-01-05 20:14:54 -0600

    Update classes of home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,39 +4,53 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="homeImagesContainer">
-      <img
-        src={require("../assets/brent.jpg").default}
-        alt="Brent Aronsen"
-        className="homeImageMain"
-      ></img>
-      <Link to="/books">
+      <div className="homeImageMainContainer">
         <img
-          src={require("../assets/acrobatics-1.jpg").default}
-          alt="Books"
-          className="homeImage1 homeImageSub"
-        ></img>
-      </Link>
-      <Link to="/games">
-        <img
-          src={require("../assets/acrobatics-2.jpg").default}
-          alt="Crossword"
-          className="homeImage2 homeImageSub"
-        ></img>
-      </Link>
-      <Link to="/acrobatics">
-        <img
-          src={require("../assets/acrobatics-3.jpg").default}
-          alt="Acrobatics"
-          className="homeImage3 homeImageSub"
-        ></img>
-      </Link>
-      <Link to="/development">
-        <img
-          src={require("../assets/acrobatics-4.jpg").default}
-          alt="Computer"
-          className="homeImage4 homeImageSub"
-        ></img>
-      </Link>
+          src={require("../assets/brent.jpg").default}
+          alt="Brent Aronsen"
+          className="homeImageMain"
+        />
+      </div>
+      <div className="homeImageSubContainer1 homeImageSubContainer">
+        <Link to="/books">
+          <img
+            src={require("../assets/acrobatics-1.jpg").default}
+            alt="Books"
+            className="homeImageSub"
+          ></img>
+        <p className="homeImageSubText">Books</p>
+        </Link>
+      </div>
+      <div className="homeImageSubContainer2 homeImageSubContainer">
+        <Link to="/games">
+          <img
+            src={require("../assets/acrobatics-2.jpg").default}
+            alt="Crossword"
+            className="homeImageSub"
+          ></img>
+          <p className="homeImageSubText">Games</p>
+        </Link>
+      </div>
+      <div className="homeImageSubContainer3 homeImageSubContainer">
+        <Link to="/acrobatics">
+          <img
+            src={require("../assets/acrobatics-3.jpg").default}
+            alt="Acrobatics"
+            className="homeImageSub"
+          ></img>
+          <p className="homeImageSubText">Acrobatics</p>
+        </Link>
+      </div>
+      <div className="homeImageSubContainer4 homeImageSubContainer">
+        <Link to="/development">
+          <img
+            src={require("../assets/acrobatics-4.jpg").default}
+            alt="Computer"
+            className="homeImageSub"
+          ></img>
+          <p className="homeImageSubText">Web Development</p>
+        </Link>
+      </div>
     </div>
   );
 };
