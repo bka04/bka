9e4408e704b5b51ece99f0bc0c93b7ba62e80caa
Brent Aronsen <bkaronsen@gmail.com>
2022-03-12 18:40:34 -0600

    Add tooltips to power ups

--- src/components/Games/CrosswordPowerUps.js
+++ src/components/Games/CrosswordPowerUps.js
@@ -1,25 +1,28 @@
 import "./CrosswordPowerUps.css";
 
 const icons = [
-  {id: "die", alt: "Reveal Random Letter"},
-  {id: "k", alt: "Reveal Letter"},
-  {id: "checkmark", alt: "Verify Word"},
-  {id: "buyLetter", alt: "Reveal Letter Everywhere"},
-  {id: "check", alt: "Verify Grid"},
-  {id: "word", alt: "Reveal Word"}
+  {id: "checkmark", text: "Verify Word"},
+  {id: "die", text: "Reveal Random Letter"},
+  {id: "k", text: "Reveal Letter"},
+  {id: "buyLetter", text: "Reveal Letter Everywhere"},
+  {id: "check", text: "Verify Grid"},
+  {id: "word", text: "Reveal Word"}
 ];
 
 const crosswordPowerUps = (props) => {
   return (
     <div className="crosswordPowerUps">
       {icons.map((icon) => (
+        <div className="crosswordPowerUp">
         <img 
           src={require("../../assets/icon-" + icon.id + ".png").default}
-          alt={icon.alt}
+          alt={icon.text}
           width="50"
           height="50"
           key={icon.id}
         />
+        <span className='tooltiptext'>{icon.text}</span>
+        </div>
       ))}
     </div>
   )
