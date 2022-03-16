0a33014d5b35a40a3da86eac353a77f87086589b
brent-a <brent.k.aronsen@emcins.com>
2022-03-16 15:38:13 -0500

    Clean up repetitive html

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
 import "./Home.css";
 
+const pages = [
+  { path: "/crosswords", class: "homeImageSubContainer1", img: "acrobatics-1.jpg", text: "Crosswords"},
+  { path: "/books", class: "homeImageSubContainer2", img: "acrobatics-2.jpg", text: "Books"},
+  { path: "/acrobatics", class: "homeImageSubContainer3", img: "acrobatics-3.jpg", text: "Acrobatics"},
+  { path: "/development", class: "homeImageSubContainer4", img: "acrobatics-4.jpg", text: "Web Development"},
+];
+
 const Home = () => {
   return (
     <div className="homeImagesContainer">
@@ -11,46 +18,18 @@ const Home = () => {
           className="homeImageMain"
         />
       </div>
-      <div className="homeImageSubContainer1 homeImageSubContainer">
-        <Link to="/books">
-          <img
-            src={require("../assets/acrobatics-1.jpg").default}
-            alt="Books"
-            className="homeImageSub"
-          ></img>
-        <p className="homeImageSubText">Books</p>
-        </Link>
-      </div>
-      <div className="homeImageSubContainer2 homeImageSubContainer">
-        <Link to="/crosswords">
-          <img
-            src={require("../assets/acrobatics-2.jpg").default}
-            alt="Crossword"
-            className="homeImageSub"
-          ></img>
-          <p className="homeImageSubText">Crosswords</p>
-        </Link>
-      </div>
-      <div className="homeImageSubContainer3 homeImageSubContainer">
-        <Link to="/acrobatics">
-          <img
-            src={require("../assets/acrobatics-3.jpg").default}
-            alt="Acrobatics"
-            className="homeImageSub"
-          ></img>
-          <p className="homeImageSubText">Acrobatics</p>
-        </Link>
-      </div>
-      <div className="homeImageSubContainer4 homeImageSubContainer">
-        <Link to="/development">
-          <img
-            src={require("../assets/acrobatics-4.jpg").default}
-            alt="Computer"
-            className="homeImageSub"
-          ></img>
-          <p className="homeImageSubText">Web Development</p>
-        </Link>
-      </div>
+      {pages.map((page) => (
+        <div key="path" className={`homeImageSubContainer ${page.class}`}>
+          <Link to={page.path}>
+            <img
+              src={require(`../assets/${page.img}`).default}
+              alt={page.text}
+              className="homeImageSub"
+            ></img>
+            <p className="homeImageSubText">{page.text}</p>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
