aca4c6d6d6aea5fe2cc26d4c3ac955936f87a370
Brent Aronsen <bkaronsen@gmail.com>
2022-03-16 21:36:29 -0500

    Update home page layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,19 +5,19 @@ const pages = [
   { path: "/crosswords", class: "homeImageSubContainer1", img: "crosswordcat.jpg", text: "Crosswords"},
   { path: "/books", class: "homeImageSubContainer2", img: "books.jpg", text: "Books"},
   { path: "/acrobatics", class: "homeImageSubContainer3", img: "acrobatics-3.jpg", text: "Acrobatics"},
-  { path: "/development", class: "homeImageSubContainer4", img: "acrobatics-4.jpg", text: "Web Development"},
+  { path: "/development", class: "homeImageSubContainer4", img: "brent.jpg", text: "Web Development"},
 ];
 
 const Home = () => {
   return (
     <div className="homeImagesContainer">
-      <div className="homeImageMainContainer">
+      {/* <div className="homeImageMainContainer">
         <img
           src={require("../assets/brent.jpg").default}
           alt="Brent Aronsen"
           className="homeImageMain"
         />
-      </div>
+      </div> */}
       {pages.map((page) => (
         <div key="path" className={`homeImageSubContainer ${page.class}`}>
           <Link to={page.path}>
