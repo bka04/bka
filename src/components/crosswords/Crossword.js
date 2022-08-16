8ab29398b647b5f2eca847f61b8f6e27add72b56
brent-a <brent.k.aronsen@emcins.com>
2022-08-16 11:58:10 -0500

    Split out crossword methods into separate file

--- src/components/crosswords/Crossword.js
+++ src/components/crosswords/Crossword.js
@@ -7,67 +7,7 @@ import CrosswordGrid from "./CrosswordGrid";
 import CrosswordPowerUps from "./CrosswordPowerUps";
 import './Crossword.css';
 import CrosswordActiveClue from "./CrosswordActiveClue";
-
-
-//Populate the across and down question numbers for each cell
-//Also determine which cells need to display a question number
-const populateNumbers = (data) => {
-  const COLS = Math.sqrt(data.length);
-
-  const newDownWord = (i) => {
-    if (i - COLS < 0) {
-      return true; //top row will have new numbers
-    }
-    if (data[i - COLS].disabled) {
-      return true; //if cell above is disabled, need new number
-    }
-    return false;
-  }; //end newDownWord
-
-  const newAcrossWord = (i) => {
-    if (i - 1 < 0) {
-      return true; //first cell needs new number
-    }
-    if (i % COLS === 0) {
-      return true; //first column needs new numbers
-    }
-    if (data[i - 1].disabled) {
-      return true; //if cell to the left is disabled, need new number
-    }
-    return false;
-  }; //end newAcrossWord
-
-  let questionNum = 0;
-
-  for (let i = 0; i < data.length; i++) {
-    data[i].questionNumberDisplayed = 0;
-
-    if (data[i].disabled) {
-      continue;
-    }
-
-    const newDown = newDownWord(i);
-    const newAcross = newAcrossWord(i);
-
-    if (newDown || newAcross) {
-      questionNum++;
-      data[i].questionNumberDisplayed = questionNum; //Set number to be displayed in cell
-    }
-
-    if (newDown) {
-      data[i].questionNumberDown = questionNum; //New down number
-    } else {
-      data[i].questionNumberDown = data[i - COLS].questionNumberDown; //Get down number for cell above
-    }
-    if (newAcross) {
-      data[i].questionNumberAcross = questionNum; //New across number
-    } else {
-      data[i].questionNumberAcross = data[i - 1].questionNumberAcross; //Get down number for cell to the left
-    }
-  }
-
-  return data;
-}; //end populateNumbers
+import crosswordMethods from "./CrosswordMethods";
 
 const initialState = {
   cellData: [],
@@ -76,180 +16,6 @@ const initialState = {
   cols: 0,
   solved: false
 };
-
-const updateHighlighting = (state, index) => {
-  const questionNumberProp = state.across
-    ? "questionNumberAcross"
-    : "questionNumberDown";
-  const currentQuestion = state.cellData[index][questionNumberProp];
-
-  state.cellData.forEach((el, index) => {
-    if (el[questionNumberProp] === currentQuestion) {
-      state.cellData[index].highlight = true;
-    }
-  });
-
-  return state.cellData;
-};
-
-//reset highlight and focus for all cells
-const clearCellDisplay = (cellData) => {
-  return cellData.map((cell) => ({
-    ...cell,
-    highlight: false,
-    focus: false,
-  }));
-};
-
-const clearCellValues = (cellData) => {
-  return cellData.map((cell) => ({
-    ...cell,
-    value: ''
-  }));
-}
-
-const getNextCell = (state, index, directionOverride = "") => {
-  const TOTALCELLS = Math.pow(state.cols, 2);
-  if (
-    (state.across || directionOverride === "across") &&
-    directionOverride !== "down"
-  ) {
-    index++;
-    if (index === TOTALCELLS) {
-      index = 0;
-    }
-    while (index < TOTALCELLS) {
-      if (!state.cellData[index].disabled) {
-        break;
-      }
-      index++;
-      if (index === TOTALCELLS - 1) {
-        index = 0;
-      }
-    }
-  } else {
-    //down
-    index += state.cols;
-    if (index === TOTALCELLS - 1 + state.cols) {
-      index = 0;
-    } else if (index > TOTALCELLS - 1) {
-      index -= TOTALCELLS - 1;
-    }
-    while (index < TOTALCELLS) {
-      if (!state.cellData[index].disabled) {
-        break;
-      }
-      index += state.cols;
-      if (index > TOTALCELLS - 1) {
-        index -= TOTALCELLS - 1;
-      }
-    }
-  }
-  return index;
-};
-
-const getPrevCell = (state, index, directionOverride = "") => {
-  const TOTALCELLS = Math.pow(state.cols, 2);
-  if (
-    (state.across || directionOverride === "across") &&
-    directionOverride !== "down"
-  ) {
-    index--;
-    if (index < 0) {
-      index = TOTALCELLS - 1;
-    }
-    while (index >= 0) {
-      if (!state.cellData[index].disabled) {
-        break;
-      }
-      index--;
-      if (index < 0) {
-        index = TOTALCELLS - 1;
-      }
-    }
-  } else {
-    //down
-    index -= state.cols;
-    if (index === 0 - state.cols) {
-      index = TOTALCELLS - 1;
-    } else if (index < 0) {
-      index += TOTALCELLS - 1;
-    }
-    while (index > 0) {
-      if (!state.cellData[index].disabled) {
-        break;
-      }
-      index -= state.cols;
-      if (index < 0) {
-        index += TOTALCELLS - 1;
-      }
-    }
-  }
-  return index;
-};
-
-const getNextWord = (state, index) => {
-  const questionNumberProp = state.across
-  ? "questionNumberAcross"
-  : "questionNumberDown";
-  const currentQuestionNumber = state.cellData[index][questionNumberProp];
-
-  const maxQuestionNumber = Math.max.apply(null, state.cellData.map(cell => cell[questionNumberProp] ? cell[questionNumberProp] : 0));
-  
-  if (currentQuestionNumber === maxQuestionNumber) { //if already at the highest down or across question number, get the lowest
-    const minQuestionNumber = Math.min.apply(null, state.cellData.map(cell => cell[questionNumberProp] ? cell[questionNumberProp] : 999));
-    return state.cellData.findIndex(x => x[questionNumberProp] === minQuestionNumber);
-  }
-
-  for (let q = currentQuestionNumber + 1; q <= maxQuestionNumber; q++) {
-    const newIndex = state.cellData.findIndex(x => x[questionNumberProp] === q);
-    if (newIndex > -1) {
-      return newIndex; //return if valid. Otherwise, add 1 and try again.
-    }
-  }
-}
-
-const getPrevWord = (state, index) => {
-  const questionNumberProp = state.across
-  ? "questionNumberAcross"
-  : "questionNumberDown";
-  const currentQuestionNumber = state.cellData[index][questionNumberProp];
-
-  const minQuestionNumber = Math.min.apply(null, state.cellData.map(cell => cell[questionNumberProp] ? cell[questionNumberProp] : 999));
-  
-  if (currentQuestionNumber === minQuestionNumber) { //if already at the lowest down or across question number, get the highest
-    const maxQuestionNumber = Math.max.apply(null, state.cellData.map(cell => cell[questionNumberProp] ? cell[questionNumberProp] : 0));
-    return state.cellData.findIndex(x => x[questionNumberProp] === maxQuestionNumber);
-  }
-
-  for (let q = currentQuestionNumber - 1; q >= minQuestionNumber; q--) {
-    const newIndex = state.cellData.findIndex(x => x[questionNumberProp] === q);
-    if (newIndex > -1) {
-      return newIndex; //return if valid. Otherwise, subtract 1 and try again.
-    }
-  }
-}
-
-const getSelectedWord = (state) => {
-  const direction = state.across ? "Across" : "Down"; //is across or down selected?
-  const word = state.cellData.filter(cell => //get all data on this word
-    cell[`questionNumber${direction}`] === 
-    state.cellData[state.selectedCell - 1][`questionNumber${direction}`]
-  );
-  return word;
-}
-
-const checkGridAgainstAnswers = (state, answers) => {
-  const gridLetters = state.cellData.map(cell => cell.value); //get letters from screen
-  return gridLetters.every((val, index) => val === answers[index]); //every letter correct?
-}
-
-const handleSolvedGrid = (state) => {
-  state.cellData = state.cellData.map((cell) => ({...cell, locked: true}));
-  alert("Well solved, crossworder!");
-  state.solved = true;
-  return state;
-}
 
 const reducer = (state, action) => {
   if (action.type === "loadStateFromStorage") {
@@ -272,10 +38,10 @@ const reducer = (state, action) => {
 
   if (action.type === "resetGrid") {
     localStorage.removeItem("crosswordData");
-    newState.cellData = clearCellValues(action.initialCrosswordData);
-    newState.cellData = populateNumbers(newState.cellData);
+    newState.cellData = crosswordMethods.clearCellValues(action.initialCrosswordData);
+    newState.cellData = crosswordMethods.populateNumbers(newState.cellData);
     newState.across = true;
-    newState.cellData = updateHighlighting(newState, 0);
+    newState.cellData = crosswordMethods.updateHighlighting(newState, 0);
     return {
       cellData: newState.cellData,
       selectedCell: 1,
@@ -292,9 +58,9 @@ const reducer = (state, action) => {
       cell[`questionNumber${action.event.target.dataset.direction}`] === parseInt(action.event.target.dataset.questionNumber)
     );
 
-    newState.cellData = clearCellDisplay(newState.cellData);
+    newState.cellData = crosswordMethods.clearCellDisplay(newState.cellData);
     newState.cellData[index].focus = true;
-    newState.cellData = updateHighlighting(newState, index);
+    newState.cellData = crosswordMethods.updateHighlighting(newState, index);
     
     const crosswordData = {
       cellData: newState.cellData,
@@ -319,9 +85,9 @@ const reducer = (state, action) => {
         newState.cellData[cellID - 1].value = action.answers[cellID - 1]; //set answer
         newState.cellData[cellID - 1].locked = true; //lock it
         newState.cellData[cellID - 1].wrong = false; //not wrong
-        const solved = checkGridAgainstAnswers(newState, action.answers) //has grid been solved?
+        const solved = crosswordMethods.checkGridAgainstAnswers(newState, action.answers) //has grid been solved?
         if (solved) {
-          newState = handleSolvedGrid(newState);
+          newState = crosswordMethods.handleSolvedGrid(newState);
         }
       }
     }
@@ -334,7 +100,7 @@ const reducer = (state, action) => {
           cell.id === newState.cellData[newState.selectedCell - 1].id
         ); 
       } else {
-        revealThis = getSelectedWord(newState); //selected word
+        revealThis = crosswordMethods.getSelectedWord(newState); //selected word
       }
 
       revealThis.forEach(function(cell) {
@@ -343,9 +109,9 @@ const reducer = (state, action) => {
         newState.cellData[cell.id - 1].wrong = false; //not wrong
       });
 
-      const solved = checkGridAgainstAnswers(newState, action.answers) //has grid been solved?
+      const solved = crosswordMethods.checkGridAgainstAnswers(newState, action.answers) //has grid been solved?
       if (solved) {
-        newState = handleSolvedGrid(newState);
+        newState = crosswordMethods.handleSolvedGrid(newState);
       }
 
     }
@@ -354,7 +120,7 @@ const reducer = (state, action) => {
 
       let verifyThis = []; //will be word or entire grid
       if (action.powerUp === "verifyWord") {
-        verifyThis = getSelectedWord(newState); //selected word
+        verifyThis = crosswordMethods.getSelectedWord(newState); //selected word
       } else {
         verifyThis = newState.cellData; //entire grid
       }
@@ -394,9 +160,9 @@ const reducer = (state, action) => {
       newState.across = !newState.across;
     }
 
-    newState.cellData = clearCellDisplay(newState.cellData);
+    newState.cellData = crosswordMethods.clearCellDisplay(newState.cellData);
     newState.cellData[index].focus = true;
-    newState.cellData = updateHighlighting(newState, index);
+    newState.cellData = crosswordMethods.updateHighlighting(newState, index);
 
     const crosswordData = {
       cellData: newState.cellData,
@@ -411,10 +177,10 @@ const reducer = (state, action) => {
     if (action.event.keyCode === 8 || action.event.keyCode === 46) {
       //backspace/delete
       if (newState.cellData[index].value === "" || newState.cellData[index].locked) { //if blank or locked, get the previous cell
-        index = getPrevCell(newState, index);
-        newState.cellData = clearCellDisplay(newState.cellData);
+        index = crosswordMethods.getPrevCell(newState, index);
+        newState.cellData = crosswordMethods.clearCellDisplay(newState.cellData);
         newState.cellData[index].focus = true;
-        newState.cellData = updateHighlighting(newState, index);
+        newState.cellData = crosswordMethods.updateHighlighting(newState, index);
       }      
        //handle backspace/delete
       if (!newState.cellData[index].locked) { //if locked, don't delete anything!
@@ -460,43 +226,43 @@ const reducer = (state, action) => {
       if (!newState.cellData[index].locked && newState.cellData[index].value !== action.event.key) {
         newState.cellData[index].value = action.event.key; //set entered letter
         newState.cellData[index].wrong = false; //clear out any 'wrong' line-through
-        solved = checkGridAgainstAnswers(newState, action.answers) //has grid been solved?
+        solved = crosswordMethods.checkGridAgainstAnswers(newState, action.answers) //has grid been solved?
         if (solved) { //if so, lock all letters in grid and alert user of great success
-          newState = handleSolvedGrid(newState);
+          newState = crosswordMethods.handleSolvedGrid(newState);
         }
       }
     }
 
-    newState.cellData = clearCellDisplay(newState.cellData);
+    newState.cellData = crosswordMethods.clearCellDisplay(newState.cellData);
 
     switch (action.event.keyCode) {
       case 37: //left arrow
-        index = getPrevCell(newState, index, "across");
+        index = crosswordMethods.getPrevCell(newState, index, "across");
         break;
       case 38: //up arrow
-        index = getPrevCell(newState, index, "down");
+        index = crosswordMethods.getPrevCell(newState, index, "down");
         break;
       case 39: //right arrow
-        index = getNextCell(newState, index, "across");
+        index = crosswordMethods.getNextCell(newState, index, "across");
         break;
       case 40: //down arrow
-        index = getNextCell(newState, index, "down");
+        index = crosswordMethods.getNextCell(newState, index, "down");
         break;
       case 9: //tab
         if (action.event.shiftKey) {
-          index = getPrevWord(newState, index)
+          index = crosswordMethods.getPrevWord(newState, index)
         } else {
-          index = getNextWord(newState, index);
+          index = crosswordMethods.getNextWord(newState, index);
         }
         break;
       default: //letter
         if (!solved) {
-          index = getNextCell(newState, index);
+          index = crosswordMethods.getNextCell(newState, index);
         }
     }
 
     newState.cellData[index].focus = true;
-    newState.cellData = updateHighlighting(newState, index); 
+    newState.cellData = crosswordMethods.updateHighlighting(newState, index); 
 
 
     const crosswordData = {
